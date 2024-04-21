83547966fd6df4c155547f5e31d8bf5d43df46e5
Jery2022 <jewomba@hotmail.com>
2024-04-21 12:02:48 +0100

    Ajout de la page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+/*    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    */
+    {
+        return new Response(
+            <<<EOF
+            <html>
+                <body style="background-color:powderblue;">
+                    <div style="display:flex; flex-direction:column;justify-content:center; align-items:center">
+                        <h1>Bienvenue sur <span style="color:red;">BiblioTIPPEE</span></h1>
+                        <h2>Site de gestion des documents de la CNTIPPEE </h2>
+                        <img src="/images/under-construction.gif" alt="En construction" />
+                    </div>
+                 </body>
+            </html>
+            EOF
+        );
+    }
+}
