d134412d3d2506f12828ceb2e2262bbc1f3e3327
Jery2022 <jewomba@hotmail.com>
2024-05-17 16:47:50 +0100

    modification du fichier gitignore

---
+++ public/assets/app-a1afda369d67ce53e7c8ea0ffe11936b.js
@@ -0,0 +1,13 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+//import * as bootstrap from 'bootstrap'
+import './styles/app.css';
+import './bootstrap.js';
+//import bsCustomFileInput from 'bs-custom-file-input';
+//bsCustomFileInput.init();
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
